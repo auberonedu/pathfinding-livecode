e0efbe2520e899fca77d3dcab1b749730166bb23
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-05-27 14:47:48 -0700

    Coded along in class

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class PathFinder {
 
@@ -72,10 +76,69 @@ public class PathFinder {
     // Sandymount Strand ↔ Finnegan's House (9000m detour)
     sandymountStrand.edges.add(new Edge<>(9000, finnegansHouse));
     finnegansHouse.edges.add(new Edge<>(9000, sandymountStrand));
+
+    System.out.println();
+    System.out.println();
+    System.out.println(distance(hcesHouse, finnegansHouse));
   }
 
   public static <T> int distance(Vertex<T> start, Vertex<T> end) {
-    // TODO: implement shortest‑path distance
-    return -1;
+
+    // For a priority que need to keep track of vertices we are visiting next, but need to sort them in which is closest
+    Queue<Edge<T>> minQ = new PriorityQueue<>();
+
+    // Map is used to keep track of distance from start location to every other location. Key is the next node, value is the distance
+    // The keys of a map are unique, so this can be used as our visited. If we haven't calculated the distanced of a node yet, we haven't visited it
+    Map<Vertex<T>, Integer> distances = new HashMap<>();
+
+    Map<Vertex<T>, Vertex<T>> previous = new HashMap<>();
+
+    // Adding the starting point to the queue. Has a weight of 0 since the distance to itself is 0
+    minQ.add(new Edge<>(0, start));
+
+    // Do a BFS now to see connected vertices
+    while(!minQ.isEmpty()) {
+
+      // Removing value at the front of the queue and saving it to a variable
+      Edge<T> current = minQ.poll();
+
+      // If the map has already calculated distance, skip over it
+      if (distances.containsKey(current.endpoint)) {
+        continue;
+      }
+
+      // Adding the current distance to the map
+      distances.put(current.endpoint, current.weight);
+
+      for (Edge<T> neighbor : current.endpoint.edges) {
+        // If the map doesn't contain the weight path to a node
+        if(!distances.containsKey(neighbor.endpoint)) {
+
+          int newDistance = current.weight + neighbor.weight;
+          Edge<T> newEdge = new Edge<>(newDistance, neighbor.endpoint);
+          minQ.add(newEdge);
+
+          // If the neighbor hasn't said what their previous node is
+          if(!previous.containsKey(neighbor.endpoint)) {
+            // Add the node was come from as the value
+            previous.put(neighbor.endpoint, current.endpoint);
+          }
+        }
+      }
+    }
+
+    Vertex<T> current = end;
+
+    List<T> path = new ArrayList<>();
+
+    while(current != null) {
+      path.addFirst(current.data);
+      // System.out.println(current.data);
+      current = previous.get(current);
+    }
+
+    System.out.println(path.reversed());
+
+    return distances.get(end);
   }
 }
