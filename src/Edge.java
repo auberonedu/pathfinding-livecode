2a8314dcff10e4e19e2216d0e3a94068c9e02253
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-05-27 11:50:14 -0700

    Made edge comparable

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,13 @@
-public class Edge<T> {
+public class Edge<T> implements Comparable<Edge<T>> {
   public int weight;
   public Vertex<T> endpoint;
 
   public Edge(int weight, Vertex<T> endpoint) {
     this.weight = weight;
     this.endpoint = endpoint;
+  }
+
+  public int compareTo(Edge<T> other) {
+    return Integer.compare(weight, other.weight);
   }
 }
